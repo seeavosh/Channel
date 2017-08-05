69800cebbda530b4894b31f53a4db5e4ab6ed5be
deve_maximus <30285010+CoderTM@users.noreply.github.com>
2017-08-05 23:41:40 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "431169778:AAH9Y7bhyyPhdLqxTFVTQSTJ8rg2H7QjjK0"
+bot_token = "توکن"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 311704513
+sudo_id = آیدی عددی
